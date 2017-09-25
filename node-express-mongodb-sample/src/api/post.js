3889ffd17f1f6a27bfde981e00c1496262d19b4a
Lichun Dai <a546684355@qq.com>
2017-09-25 15:52:44 +0800

    update

--- node-express-mongodb-sample/src/api/post.js
+++ node-express-mongodb-sample/src/api/post.js
@@ -2,7 +2,9 @@ const dbtest = require('../db/dbtest');
 const log = require('../util/debug').log;
 const bodyParser = require('body-parser');
 // 创建 application/x-www-form-urlencoded 编码解析
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false,
+});
 
 /**
  * 定义一些post api
@@ -15,25 +17,41 @@ module.exports.postMixmin = function postMixmin(app) {
             hello: 'world!',
         }));
     });
-    
+
     app.post('/savedata', urlencodedParser, (req, res) => {
         const userName = req.body.userName;
         const password = req.body.password;
-        
+
         if (!userName || !password) {
             res.end(JSON.stringify({
                 error: '参数格式错误',
             }));
         } else {
             dbtest.insert(userName, password, (result) => {
+                // 写入测试cookie
+                // 注意，cors里配置了一个具体的跨域域名，需要改成对应的地址
+                res.writeHead(200, {
+                    'Set-Cookie': 'myCookie=test',
+                });
                 res.end(JSON.stringify(result));
             });
         }
     });
-    
+
+    app.post('/testcookie', urlencodedParser, (req, res) => {
+        // 写入测试cookie
+        // 注意，cors里配置了一个具体的跨域域名，需要改成对应的地址
+        res.writeHead(200, {
+            'Set-Cookie': 'myCookie=test',
+        });
+        res.end(JSON.stringify({
+            success: 'hello',
+        }));
+    });
+
     app.post('/getdata', urlencodedParser, (req, res) => {
         const userName = req.body.userName;
-        
+
         if (!userName) {
             res.end(JSON.stringify({
                 error: '参数格式错误',
--- node-express-mongodb-sample/src/inner/corswithcookie.js
+++ node-express-mongodb-sample/src/inner/corswithcookie.js
@@ -6,8 +6,10 @@ const log = require('../util/debug').log;
 module.exports.corsMixmin = function corsMixmin(app) {
     // 设置跨域访问
     app.all('*', (req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
+        // 设置成对应的域名-因为开启了cookie
+        res.header('Access-Control-Allow-Origin', 'http://192.168.114.35:8020');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+        res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
         res.header('X-Powered-By', ' 3.2.1');
         
